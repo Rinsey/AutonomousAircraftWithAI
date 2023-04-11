050075ccf746b58d989c2c1d7b370fc2189bb9f5
ChristopherM <89361839+Rinsey@users.noreply.github.com>
2023-04-10 20:31:58 -0700

    Update autonomousMovement.ino

--- PADA_Code_Rev-NC/autonomousMovement.ino
+++ PADA_Code_Rev-NC/autonomousMovement.ino
@@ -3,81 +3,48 @@
 void autonomousMovement(){
   
   if(distanceObjective > 100 && relativeAltitude >= 8 && relativeAltitude < 15 && pitch > -1 && pitch < 1){ //CRUISE MODE
+    estabilizacion.setAngles(0.0,0.0); //Pitch, Roll
     Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch()); //Elevator and Ailerons always on middle point
     Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());   //Turn movement will be affected only with the rudder
     Motor.writeMicroseconds(cruiseSpeed);
   }
 
   if(distanceObjective > 100 && relativeAltitude < 8){ //ELEVATION TO AVOID ALTITUDE LOST
-    if(pitch > 6){
+    estabilizacion.setAngles(5.0,0.0);
     Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-    Elevator.writeMicroseconds(1400); //Elevator movement to increas de AOA gaining altitude
+    Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
     Motor.writeMicroseconds(cruiseSpeed);
-    } else if(pitch < 1) {
-      Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-      Elevator.writeMicroseconds(1700); //Elevator movement to increas de AOA gaining altitude
-      Motor.writeMicroseconds(cruiseSpeed);
-    }
-    if(pitch < 6 && pitch > 1) {
-      Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-      Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
-      Motor.writeMicroseconds(cruiseSpeed);
-    }
   }
 
   if(distanceObjective > 100  && relativeAltitude > 15){ //DESCENT TO AVOID GAINING ALTITUDE
-    if(pitch > -1){
-      Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-      Elevator.writeMicroseconds(1400); //Elevator movement to increas de AOA gaining altitude
-      Motor.writeMicroseconds(cruiseSpeed);
-    } else if(pitch < -5){
-      Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-      Elevator.writeMicroseconds(1700); //Elevator movement to increas de AOA gaining altitude
-      Motor.writeMicroseconds(cruiseSpeed);
-    }
-    if(pitch < -5 && pitch > -1) {
-      Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-      Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
-      Motor.writeMicroseconds(cruiseSpeed);
-    }
+    estabilizacion.setAngles(-3.0,0.0);
+    Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
+    Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
+    Motor.writeMicroseconds(cruiseSpeed);
   }
 ///////////////////////////////100 METERS TO OBJECTIVE REACHED FUNCTIONS///////////////////////////////////////////
   
-  if(distanceObjective < 100 && relativeAltitude < 8){ //Landing starts here at 100 meters away from  
-    if(pitch < -5.5){
-       Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-       Motor.writeMicroseconds(cruiseSpeed);
-       Elevator.writeMicroseconds(1300);
-    }
-    if(pitch > -4.5){
-       Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-       Motor.writeMicroseconds(cruiseSpeed);
-       Elevator.writeMicroseconds(1700);
-    }
-    if(pitch < -4.5 && pitch > -5.5){
-       Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-       Motor.writeMicroseconds(cruiseSpeed);
-    }
+  if(distanceObjective < 100 && relativeAltitude < 15){ //Landing starts here at 100 meters away from  
+    estabilizacion.setAngles(-7.0,0.0);
+    Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
+    Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
+    Motor.writeMicroseconds(cruiseSpeed - 200);  
+  }
+
+  if(distanceObjective < 100 && relativeAltitude < 3){ //Landing starts here at 100 meters away from  
+    estabilizacion.setAngles(2.0,0.0);
+    Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
+    Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
+    Motor.writeMicroseconds(cruiseSpeed);
   }
 
 ///////////////////////////////100 METERS TO OBJECTIVE REACHED FUNCTIONS///////////////////////////////////////////
   
-  if(distanceObjective < 10 && relativeAltitude < 3){ //Landing starts here at 100 meters away from  
-    if(pitch < -5.5){
-       Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-       Motor.writeMicroseconds(1000);
-       Elevator.writeMicroseconds(1300);
-    }
-    if(pitch > -4.5){
-       Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-       Motor.writeMicroseconds(1000);
-       Elevator.writeMicroseconds(1700);
-    }
-    if(pitch < -4.5 && pitch > -5.5){
-       Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
-       Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
-       Motor.writeMicroseconds(1000);
-    }
+  if(distanceObjective < 10 && relativeAltitude < 15){ //Landing starts here at 100 meters away from  
+    estabilizacion.setAngles(10.0,0.0);
+    Aileron.writeMicroseconds(1500 + estabilizacion.outputsRoll());
+    Elevator.writeMicroseconds(1500 - estabilizacion.outputsPitch());
+    Motor.writeMicroseconds(1000); //Motor se apaga
   }
 
 //////////////////////////////RUDDER CORRECTIONS (BEARING = HEADING)///////////////////////////////////////////////////////////////////
